01c83880c8f98804d62d1649588a3d362774407b
John Argie T. Hista <152931511+janzzera@users.noreply.github.com>
2025-07-19 08:38:03 +0000

    fix43

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -16,11 +16,11 @@ class EventFactory extends Factory
      */
     public function definition(): array
     {
-        $images = 'storage/events/hackaton.jpg';
+
         return [
             'title' => $this->faker->sentence(3),
             'description' => $this->faker->paragraph(),
-            'imageurl' => $images,
+            'imageurl' => '/storage/events/hackaton.jpg',
             'date' => $this->faker->dateTimeBetween('-1 month', '+3 months'),
         ];
     }
